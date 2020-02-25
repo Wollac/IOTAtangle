4bbb9a1c370444d0a46263d5e6a5325c93214e89
Wolfgang Welz <welzwo@gmail.com>
2020-02-25 21:36:38 +0100

    add docker support

--- mainnetApp.js
+++ mainnetApp.js
@@ -1,11 +1,11 @@
 const Tangle = require('./app.js')
 
 const NAME = 'mainnet' // name used for data file NAMEdata.json
-const LSM_NODE = 'https://<CHANGEME>:443' // latest solid milestone node. sed for getting coordinator tx
-const ZMQ_ENDPOINT = 'tcp://<CHANGEME>:5556' // used to get a livestream of all tx
-const WEB_PORT = 80 // port the webserver will listen on
+const LSM_NODE = process.env.LSM_NODE
+const ZMQ_ENDPOINT = process.env.ZMQ_ENDPOINT
+const WEB_PORT = 8080 // port the webserver will listen on
 const FRONTEND_CONFIG = {
-    networkName: 'main net' // displayed in the frontend
+    networkName: 'compass' // displayed in the frontend
 }
 
 
